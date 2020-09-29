0878ed71cf288656b22dff9dad6c727a3aad78ff
csjohn1 <csjohn@shaw.ca>
2020-09-29 23:37:55 +0000

    added URL deletion funtion

--- express_server.js
+++ express_server.js
@@ -46,6 +46,11 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+app.post(`/urls/:shortURL/delete`, (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls/");
+});
+
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
