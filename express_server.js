748dd5180941912f79b1275def2ad331ce4f0fbd
csjohn1 <csjohn@shaw.ca>
2020-10-02 21:03:23 +0000

    cleaned up for initial submission

--- express_server.js
+++ express_server.js
@@ -4,7 +4,7 @@ const PORT = 8080;
 const bodyParser = require("body-parser");
 const cookieSession = require("cookie-session");
 const bcrypt = require('bcrypt');
-const { getUserByEmail, checkEmail } = require("./helpers");
+const { checkEmail } = require("./helpers");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
@@ -15,31 +15,22 @@ app.use(cookieSession({
   maxAge: 24 * 60 * 60 * 1000 // 24 hours
 }));
 
+// Creates a random 6 character string for URLs and user ID purposes
 function genShortURL() {
   return Math.random().toString(36).substr(2, 6);
 }
 
 const urlDatabase = {
-
 };
 
 const users = {
-  // "userRandomID": {
-  //   id: "userRandomID",
-  //   email: "user@example.com",
-  //   password: "1"
-  // },
-  // "user2RandomID": {
-  //   id: "user2RandomID",
-  //   email: "user2@example.com",
-  //   password: "dishwasher-funk"
-  // }
 };
 
+// Displays Hello! on main page (/)
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
-
+// Logs into terminal once server is active and listening
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
@@ -47,11 +38,11 @@ app.listen(PORT, () => {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
-
+// Displays Hello World on hello page (/hello)
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
-
+// Displays any relevant long and short URLs and permits editing and deletion of URLs
 app.get("/urls", (req, res) => {
   let userURLs = {};
   for (const url in urlDatabase) {
@@ -62,13 +53,13 @@ app.get("/urls", (req, res) => {
   const templateVars = {urls: userURLs, user: users[req.session.user_id]};
   templateVars.user ? res.render("urls_index", templateVars) : res.redirect("/login");
 });
-
+// Creates a new short URL using the genShortURL function and adds it to the database
 app.post("/urls", (req, res) => {
   let shortURL = genShortURL();
   urlDatabase[shortURL] =  {longURL: req.body.longURL, userID: req.session.user_id};
   res.redirect(`/urls/${shortURL}`);
 });
-
+// Displays form with which to create a new short URL
 app.get("/urls/new", (req, res) => {
   if (!req.session.user_id) {
     return res.redirect("/login");
@@ -78,7 +69,7 @@ app.get("/urls/new", (req, res) => {
   };
   res.render("urls_new", templateVars);
 });
-
+// Deletes the targeted short URL
 app.post(`/urls/:shortURL/delete`, (req, res) => {
   if (req.session.user_id === urlDatabase[req.params.shortURL].userID) {
     delete urlDatabase[req.params.shortURL];
@@ -87,32 +78,31 @@ app.post(`/urls/:shortURL/delete`, (req, res) => {
     return res.redirect("/login");
   }
 });
-
+// Redirects user to the targeted web page
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL]["longURL"];
   res.redirect(longURL);
 });
-
+// Displays the selected short URL, redirected unauthorized users elsewhere
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
     user: users[req.session.user_id]
   };
-  
   if (!templateVars.user) {
     return res.redirect("/login");
+  } else if (users[req.session.user_id].id !== urlDatabase[req.params.shortURL].userID) {
+    return res.status(403).send("No access");
   } else {
-  
     const templateVars = {
       shortURL: req.params.shortURL,
       longURL: urlDatabase[req.params.shortURL]["longURL"],
       user: users[req.session.user_id]
     };
-    //res.render("urls_show", templateVars);
     templateVars.user ? res.render("urls_show", templateVars) : res.redirect("/login");
   }
 });
-
+// Edits the existing long URL contained within the stated short URL
 app.post("/urls/:shortURL", (req, res) => {
   if (req.session.user_id === urlDatabase[req.params.shortURL].userID) {
     urlDatabase[req.params.shortURL].longURL = req.body.longURL;
@@ -121,7 +111,7 @@ app.post("/urls/:shortURL", (req, res) => {
     res.redirect("/login");
   }
 });
-
+// Allows users to log in if their credentials exist and are corrects, redirects/displays error otherwise
 app.post("/login", (req, res) => {
   if (req.body.email === "" || req.body.password === "") {
     return res.status(403).send("Please enter a valid email and/or password");
@@ -135,21 +125,20 @@ app.post("/login", (req, res) => {
     }
   }
   return res.status(403).send("Email and/or password not found");
-  
 });
-
+// Allows user to log out - deletes cookies as well
 app.post("/logout", (req, res) => {
   req.session.user_id = null;
   res.redirect("/urls/");
 });
-
+// Creates a registration page
 app.get("/register", (req, res) => {
   const templateVars = {
     user: users[req.session.user_id]
   };
   res.render("urls_reg", templateVars);
 });
-
+// Allows user to register for account if credentials are valid and new, redirects/displays error if otherwise
 app.post("/register", (req, res) => {
   let newID = genShortURL();
   if (req.body.email === "" || req.body.password === "") {
@@ -168,7 +157,7 @@ app.post("/register", (req, res) => {
   req.session.user_id = newID;
   res.redirect("/urls/");
 });
-
+// Creates a login page
 app.get("/login", (req, res) => {
   const templateVars = {
     user: users[req.session.user_id]
